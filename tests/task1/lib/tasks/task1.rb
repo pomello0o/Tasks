4abdd31e5a56ed21b076bcc9340a017e336c5f72
Myroslav Trynoha <trynoga.myroslav40@gmail.com>
2022-02-08 12:42:35 +0200

    tests

--- tests/task1/lib/tasks/task1.rb
+++
@@ -1,3 +0,0 @@
-def get_volume_of_cuboid(length, width, height)
-    length * width * height
-end
--- tests/task1/spec/lib/tasks/task1_spec.rb
+++
@@ -1,8 +0,0 @@
-RSpec.describe "get_volume_of_cuboid" do
-    it "returns correct volume value" do
-        expect(get_volume_of_cuboid(1,1,1)).to eq 1
-        expect(get_volume_of_cuboid(0,2,3)).to eq 0
-        expect(get_volume_of_cuboid(2,2,2)).to eq 8
-        expect(get_volume_of_cuboid(12345, 67890, 123456)).to eq 103_468_726_684_800
-    end
-end
